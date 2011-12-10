16bb8e396c94e297aba6cc507162d9f4ff3ea837
Kirill Shashkov <shashkov.kirill@gmail.com>
2011-12-10 16:15:18 +0200

    Add ImageLoader helper class

---
+++ ImageLoader.m
@@ -0,0 +1,61 @@
+//
+//  ImageLoader.m
+//
+//  Created by Kirill Shashkov on 12/2/11.
+//  Copyright (c) 2011 IntexSoft LLC. All rights reserved.
+//
+
+#import "ImageLoader.h"
+#import "UIImage+Resize.m"
+
+@implementation ImageLoader
+
+static int instanceCount = 0;
+
+- (id)init {
+    if (self = [super init]) {
+        imagesCache = [[NSCache alloc] init];
+        
+        loadingQueue = dispatch_queue_create([[NSString stringWithFormat:@"de.pz.pzipad.imageLoader[%d]", instanceCount++] UTF8String], NULL);
+    }
+    return self;
+}
+
+- (void)displayImage:(NSString *)imagePath inImageView:(UIImageView *)imageView {    
+    [self displayImage:imagePath inSize:imageView.bounds.size contentMode:imageView.contentMode callbackTarget:imageView callbackSelector:@selector(setImage:)];    
+}
+
+- (void)displayImage:(NSString *)imagePath inSize:(CGSize)size contentMode:(UIViewContentMode)contentMode callbackTarget:(id)target callbackSelector:(SEL)selector {
+    UIImage *imageFromCache = [imagesCache objectForKey:imagePath];
+    
+    if (!imageFromCache) {
+        dispatch_async(loadingQueue, ^{
+            UIImage *image = [UIImage imageWithContentsOfFile:imagePath];
+            [imagesCache setObject:image forKey:imagePath];
+            
+            UIImage *resizedImage = [image resizedImageWithContentMode:contentMode bounds:size interpolationQuality:kCGInterpolationHigh];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [target performSelector:selector withObject:resizedImage];
+            });
+        });
+    } else {
+        dispatch_async(loadingQueue, ^{
+            UIImage *resizedImage = [imageFromCache resizedImageWithContentMode:contentMode bounds:size interpolationQuality:kCGInterpolationHigh];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [target performSelector:selector withObject:resizedImage];                
+            });
+        });
+    }
+}
+
+- (void)dealloc {    
+    dispatch_release(loadingQueue);
+    
+    [imagesCache release];
+    
+    [super dealloc];
+}
+
+@end
